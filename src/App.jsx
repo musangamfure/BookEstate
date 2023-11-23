1dfbee99174be89683193ff67630dfb4feec4427
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-11-23 16:31:33 +0200

    Add Routers for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Profile from './pages/Profile.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Offers from './pages/Offers.jsx';
+import ForgotPassword from './pages/ForgotPassword.jsx';
+
 function App() {
   return (
     <>
-      <h1>Hello Realtor</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
