501aca20fc1c433f721beef06265c7d9d7facc79
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-11-28 16:50:49 +0200

    Add edit profile page functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function Profile() {
   const navigate = useNavigate();
   const auth = getAuth();
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -12,9 +16,33 @@ function Profile() {
 
   const { name, email } = formData;
 
+  function onEdit() {}
+
+  function handleChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
   function handleLogout() {
     auth.signOut();
     navigate('/');
+  }
+
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, { displayName: name });
+
+        const docRef = doc(db, 'users', auth.currentUser.uid);
+        await updateDoc(docRef, { name });
+      }
+
+      toast.success('Profile is updated successfully!');
+    } catch (error) {
+      toast.error('Could not update the profile details');
+    }
   }
   return (
     <>
@@ -26,21 +54,33 @@ function Profile() {
               type='text'
               id='name'
               value={name}
-              disabled
-              className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6'
+              disabled={!changeDetail}
+              onChange={handleChange}
+              className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 ${
+                changeDetail && 'bg-red-200 focus:bg-red-200'
+              }`}
             />
             <input
               type='text'
               id='email'
               value={email}
-              disabled
-              className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6'
+              disabled={!changeDetail}
+              onChange={handleChange}
+              className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 ${
+                changeDetail && 'bg-red-200 focus:bg-red-200'
+              }`}
             />
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
               <p className='flex items-center'>
                 Do you want to change your name?{' '}
-                <span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>
-                  Edit
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevStste) => !prevStste);
+                  }}
+                  className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'
+                >
+                  {changeDetail ? 'Apply change' : 'Edit'}
                 </span>
               </p>
               <p
